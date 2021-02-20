004f9778db57c052259f9c1752797629dde1a5c7
edyribowo <eribowo9@gmail.com>
2021-02-20 10:23:52 +0700

    init project

---
+++ assets/configure.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('body').scrollspy({target: "nav .topnav", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("nav #myTopnav a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+  }
+}
